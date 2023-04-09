f003877139fac8992b9cbf01c77cfcc16ccb51a0
Prateek922 <prateeka922@gmail.com>
2023-04-09 20:33:40 +0530

    guest pass

--- src/views/GuestDashboard.js
+++ src/views/GuestDashboard.js
@@ -1,115 +1,75 @@
 import React from "react";
 import {
-  Card,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  Row,
-  Col,
+    Card,
+    CardBody,
+    CardFooter,
+    CardTitle,
+    Row,
+    Col
 } from "reactstrap";
-import Complaints from "../components/Complaints";
 import { Link } from 'react-router-dom';
-import "../assets/css/paper-dashboard.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 
-function Dashboard() {
-  
-  const userDetails = JSON.parse(localStorage.getItem('userDetails'))
-  const userType = localStorage.getItem('userType')
-
-  return (
-    <>
-    {userType==='admin' && userDetails?
-      <div className="content">
-        <Row>
-          <Col lg="4" md="4" sm="12">
-            <Card className="card-stats">
-              <CardBody>
+function GuestDashboard() {
+    return (
+        <>  
+            <div className="content">
                 <Row>
-                  <Col md="2" xs="5">
-                    <div className="icon-big text-center icon-warning text-warning">
-                      <i className="nc-icon nc-paper" />
-                    </div>
-                  </Col>
-                  <Col md="10" xs="7">
-                    <div className="numbers">
-                      <p className="card-category">3 active notices</p>
-                      <Link to="/admin/Notices" className="linktag" ><CardTitle tag="p">Create Notice <FontAwesomeIcon icon={faCirclePlus} style={{ marginLeft: "10px", color: "#7a7a7a", }} /></CardTitle></Link>
-                      <p />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <Link to="/admin/Notices" className="linktag" ><i className="fas fa-sync-alt" /> Click to view all </Link>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col lg="4" md="4" sm="12">
-            <Card className="card-stats">
-              <CardBody>
-                <Row>
-                <Col md="2" xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-credit-card text-primary" />
-                    </div>
-                  </Col>
-                  <Col md="10" xs="7">
-                    <div className="numbers">
-                      <p className="card-category">8 Pending Bills</p>
-                      <Link to="/admin/Bills" className="linktag" ><CardTitle tag="p">Create Bills <FontAwesomeIcon icon={faCirclePlus} style={{ marginLeft: "10px", color: "#7a7a7a", }} /></CardTitle></Link>
-                      <p />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <Link to="/admin/Bills" className="linktag" > <i className="fas fa-sync-alt" /> Click to view all </Link>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-          <Col lg="4" md="4" sm="12">
-            <Card className="card-stats">
-              <CardBody>
-                <Row>
-                <Col md="2" xs="5">
-                    <div className="icon-big text-center icon-warning">
-                      <i className="nc-icon nc-settings text-danger" />
-                    </div>
-                  </Col>
-                  <Col md="10" xs="7">
-                    <div className="numbers">
-                      <p className="card-category">4 ongoing</p>
-                      <Link to="/admin/Maintenance" className="linktag" ><CardTitle tag="p">Add Maintenance <FontAwesomeIcon icon={faCirclePlus} style={{color: "#7a7a7a", }} /></CardTitle></Link>
-                      <p />
-                    </div>
-                  </Col>
-                </Row>
-              </CardBody>
-              <CardFooter>
-                <hr />
-                <div className="stats">
-                  <Link to="/admin/Maintenance" className="linktag" ><i className="fas fa-sync-alt" /> Click to view all </Link>
-                </div>
-              </CardFooter>
-            </Card>
-          </Col>
-        </Row>
-        <Complaints />
-      </div>:
-      <div className="content">Not Authorised</div>
-      }
+                    <Col md="10">
+                        <Card className="card-user" >
+                            <div className="image" style={{height:"220px",width:"100%"}}>
+                                <img style={{width:"100%",height:"auto",borderRadius:"5px"}}
+                                    src={require("assets/img/guestPass.png")}
+                                    alt="guestPass image"
+                                />
+                            </div>
+                            <CardBody>
+                                <div className="author">
+                                    <a href="#pablo" onClick={(e) => e.preventDefault()}>
+                                        <img
+                                            alt="..."
+                                            className="avatar border-gray"
+                                            src={require("assets/img/logo-profile.png")}
+                                            style={{ width: "250px", height: "auto"}}
+                                        />
+                                        <h3 className="title">Prateek Arora</h3>
+                                    </a>
+                                </div>  
+                            </CardBody>
+                            <CardFooter>
+                                <hr />
+                                <div className="button-container">
+                                    <Row>
+                                        <Col className="ml-auto" lg="3" md="6" xs="6">
+                                            <h5>
+                                                {}<br />
+                                                <small>Room no.</small>
+                                            </h5>
+                                        </Col>
+                                        <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
+                                            <h5>
+                                                2 <br />
+                                                <small>Complaints</small>
+                                            </h5>
+                                        </Col>
+                                        <Col className="mr-auto" lg="3">
+                                            <h5>
+                                                24,6$ <br />
+                                                <small>Pending bill</small>
+                                            </h5>
+                                        </Col>
+                                    </Row>
+                                </div>
+                            </CardFooter>
+                        </Card>
+                    </Col>
 
-    </>
-  );
+
+                </Row>
+            </div>
+
+        </>
+    );
 }
 
-export default Dashboard;
+export default GuestDashboard;
