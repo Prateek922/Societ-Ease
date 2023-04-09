c4d1bec14e312a57d2cfcfa16aefedfd7fc1b792
Prashant Kumar Singh <prashantrkt2002@gmail.com>
2023-04-09 21:13:18 +0530

    resident dashboard

--- src/views/ResidentDashboard.js
+++ src/views/ResidentDashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
     Card,
     CardBody,
@@ -8,12 +8,128 @@ import {
     Col
 } from "reactstrap";
 import { Link } from 'react-router-dom';
-
+import { getMyBills } from "api/Bills/billApi";
+import { getMyComplaints } from "api/Complaint/complaintApi";
+import { getNotice } from "api/Notice/noticeApi";
+import { getMaintenance } from "api/Maintenance/maintenanceApi";
 
 function ResidentDashboard() {
     const residentData = JSON.parse(localStorage.getItem('userDetails'))
     const userType = localStorage.getItem('userType')
-    
+    const [complaintCount, setComplaintCount] = useState(0);
+    const [noticeCount, setNoticeCount] = useState(0);
+    const [maintenanceCount, setMaintenanceCount] = useState(0);
+    const [catBill, setCatBill] = useState([
+        {
+          billRoomNumber: "",
+          electricity_Dues: 0,
+          water_Dues: 0,
+          maintenance_Dues: 0,
+          wifi_Dues: 0,
+          total_Dues: 0
+        }
+    ])
+
+    const groupBillsByRoomNumber = (bills) => {
+    const mp = new Map();
+        bills.map((bill) => {
+            if (bill.billType === 'Water') {
+                if (mp.has(bill.billRoomNumber)) {
+                    let obj = mp.get(bill.billRoomNumber);
+                    mp.set(bill.billRoomNumber, { ...obj, water_Dues: bill.billDue, total_Dues: obj.total_Dues + bill.billDue })
+                } else {
+                    mp.set(bill.billRoomNumber, { billRoomNumber: bill.billRoomNumber, water_Dues: bill.billDue, total_Dues: bill.billDue });
+                }
+            } else if (bill.billType === 'Maintenance') {
+                if (mp.has(bill.billRoomNumber)) {
+                    let obj = mp.get(bill.billRoomNumber);
+                    mp.set(bill.billRoomNumber, { ...obj, maintenance_Dues: bill.billDue, total_Dues: obj.total_Dues + bill.billDue })
+                } else {
+                    mp.set(bill.billRoomNumber, { billRoomNumber: bill.billRoomNumber, maintenance_Dues: bill.billDue, total_Dues: bill.billDue });
+                }
+            } else if (bill.billType === 'Electricity') {
+                if (mp.has(bill.billRoomNumber)) {
+                    let obj = mp.get(bill.billRoomNumber);
+                    mp.set(bill.billRoomNumber, { ...obj, electricity_Dues: bill.billDue, total_Dues: obj.total_Dues + bill.billDue })
+                } else {
+                    mp.set(bill.billRoomNumber, { billRoomNumber: bill.billRoomNumber, electricity_Dues: bill.billDue, total_Dues: bill.billDue });
+                }
+            } else if (bill.billType === 'Wifi') {
+                if (mp.has(bill.billRoomNumber)) {
+                    let obj = mp.get(bill.billRoomNumber);
+                    mp.set(bill.billRoomNumber, { ...obj, wifi_Dues: bill.billDue, total_Dues: obj.total_Dues + bill.billDue })
+                } else {
+                    mp.set(bill.billRoomNumber, { billRoomNumber: bill.billRoomNumber, wifi_Dues: bill.billDue, total_Dues: bill.billDue });
+                }
+            }
+        })
+
+        let fl = []
+        for (const [key, value] of mp) {
+            fl.push(value)
+        }
+        console.log(fl)
+
+        setCatBill(fl)
+    }
+
+    const countUnresolvedComplaints = (response)=>{
+        const complaints = response.myComplaints;
+        let count = 0;
+        complaints.map((comp)=>{
+            if(comp.complaintStatus!=="Resolved"){
+                count++;
+            }
+        })
+
+        setComplaintCount(count);
+    }
+
+    const fetchAllComplaints = async () => {
+        const response = await getMyComplaints();
+        if (response.success) {
+          console.log(response);
+          countUnresolvedComplaints(response)
+        } else {
+          console.log(response);
+        }
+    }
+
+    const fetchAllBills = async () => {
+        const response = await getMyBills();
+        if (response.success) {
+          console.log(response)
+          groupBillsByRoomNumber(response.bills)
+          console.log(catBill[0])
+        } else console.log(response)
+    }
+
+    const fetchAllNotices = async () => {
+        const response = await getNotice();
+        if (response.success) {
+            console.log(response);
+            setNoticeCount(response.notices.length);
+        } else {
+            console.log(response);
+        }
+    }
+
+    const fetchAllMaintenance = async () => {
+        const response = await getMaintenance();
+        if (response.success) {
+          console.log(response);
+          setMaintenanceCount(response.maintenance.length)
+        } else {
+          console.log(response);
+        }
+      }
+
+    useEffect(() => {
+        fetchAllBills();
+        fetchAllComplaints();
+        fetchAllNotices();
+        fetchAllMaintenance();
+    }, [])
 
     return (
         <>  
@@ -54,13 +170,13 @@ function ResidentDashboard() {
                                         </Col>
                                         <Col className="ml-auto mr-auto" lg="4" md="6" xs="6">
                                             <h5>
-                                                2 <br />
+                                                {complaintCount}<br />
                                                 <small>Complaints</small>
                                             </h5>
                                         </Col>
                                         <Col className="mr-auto" lg="3">
                                             <h5>
-                                                24,6$ <br />
+                                                {catBill[0].total_Dues} <br />
                                                 <small>Pending bill</small>
                                             </h5>
                                         </Col>
@@ -82,7 +198,7 @@ function ResidentDashboard() {
                                             </Col>
                                             <Col md="10" xs="7">
                                                 <div className="numbers">
-                                                    <p className="card-category">3 active notices</p>
+                                                    <p className="card-category">{noticeCount} Active Notices</p>
                                                     <Link to="/resident/Notices" className="linktag" ><CardTitle tag="p">View Notices </CardTitle></Link>
                                                     <p />
                                                 </div>
@@ -108,7 +224,7 @@ function ResidentDashboard() {
                                             </Col>
                                             <Col md="10" xs="7">
                                                 <div className="numbers">
-                                                    <p className="card-category">3 active notices</p>
+                                                    <p className="card-category">{maintenanceCount} Maintenance</p>
                                                     <Link to="/resident/Maintenance" className="linktag" ><CardTitle tag="p">Maintenance   </CardTitle></Link>
                                                     <p />
                                                 </div>
@@ -134,7 +250,7 @@ function ResidentDashboard() {
                                             </Col>
                                             <Col md="10" xs="7">
                                                 <div className="numbers">
-                                                    <p className="card-category">3 active notices</p>
+                                                    <p className="card-category">{catBill[0].total_Dues} Dues Pending</p>
                                                     <Link to="/resident/Bills" className="linktag" ><CardTitle tag="p">Bills </CardTitle></Link>
                                                     <p />
                                                 </div>
@@ -160,7 +276,7 @@ function ResidentDashboard() {
                                             </Col>
                                             <Col md="10" xs="7">
                                                 <div className="numbers">
-                                                    <p className="card-category">3 active notices</p>
+                                                    <p className="card-category">{complaintCount} Unresolved Complaints</p>
                                                     <Link to="/resident/Complaints" className="linktag" ><CardTitle tag="p">Post complaint   </CardTitle></Link>
                                                     <p />
                                                 </div>
